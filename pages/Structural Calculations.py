601fde121bb87fbc2cac859031689c18dd62d3fc
A Ranjan <68651277+Ashu-A@users.noreply.github.com>
2024-05-29 03:20:44 +0100

    Update Structural Calculations.py

--- pages/Structural Calculations.py
+++ pages/Structural Calculations.py
@@ -71,6 +71,6 @@ st.write("Deflection Check:", deflection_check(allowable_deflection, applied_loa
 st.markdown(
     """
     ---
-    Made with ❤️ by [Ashish](https://ashu-a.github.io/Ashish_portfolio/)
+    Made with ❤️ by [Ashish Ranjan](https://ashu-a.github.io/Ashish_portfolio/)
     """
 )

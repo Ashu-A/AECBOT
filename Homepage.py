4589b94396c6b39cca97898e7abe25c87dfe05f5
A Ranjan <68651277+Ashu-A@users.noreply.github.com>
2024-05-29 02:27:29 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -9,9 +9,6 @@ header = st.container()
 with header:
     st.title('Island Chatbot')
     st.info('Hi I am SEPI a virtual assistant for AEC professionals')
-# st.image('team.jpeg', caption='Team Island 2024', width=600)
-# st.sidebar.success('select a page above.')
-
 
 # Footer
 st.markdown(
